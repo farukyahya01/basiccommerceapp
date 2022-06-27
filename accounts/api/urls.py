5c1cb9eed1e8899794d8d7517be7dcbd73bd0d16
farukyahya01 <ifarukaksu@gmail.com>
2022-06-27 21:50:42 +0300

    *back to generics viewset for profile app

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from accounts.api.views import ProfileViewSet
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register(r'profile', ProfileViewSet)
+from accounts.api import views as api_views
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('profile/', api_views.ProfileListView.as_view(), name='profile'),
+    path('profile/detail/<int:pk>', api_views.ProfileDetailView.as_view(), name='profile_detail'),
 ]
--- accounts/api/views.py
+++ accounts/api/views.py
@@ -2,13 +2,10 @@ from django.shortcuts import render, redirect
 from requests import request
 
 # DRF
-from rest_framework import generics, status, mixins
+from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.generics import get_object_or_404
-# from rest_framework.viewsets import ReadOnlyModelViewSet
-from rest_framework.viewsets import GenericViewSet
 from accounts.api.serializers import ProfileSerializer
-
 # Models
 from django.contrib.auth.models import User
 from accounts.models import Profile
@@ -17,21 +14,16 @@ from rest_framework import permissions
 from accounts.api.permissions import IsProfileRecordOwner
 from rest_framework.authentication import TokenAuthentication
 
-class ProfileViewSet(
-    mixins.ListModelMixin,
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    GenericViewSet
-):
+class ProfileListView(generics.ListAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+    # def get_queryset(self):
+    #     return Profile.objects.filter(user=self.request.user.id)
+    
+
+class ProfileDetailView(generics.RetrieveUpdateAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [permissions.IsAuthenticated, IsProfileRecordOwner]
-
-    # def get_queryset(self):
-    #     return 
-    
-
-# class ProfileDetailView(generics.RetrieveUpdateAPIView):
-#     queryset = Profile.objects.all()
-#     serializer_class = ProfileSerializer
-#     permission_classes = [permissions.IsAuthenticated, IsProfileRecordOwner]
