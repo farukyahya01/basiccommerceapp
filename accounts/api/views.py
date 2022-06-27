9e4377dabd0e6d6a1343a49499fced71ba331461
farukyahya01 <ifarukaksu@gmail.com>
2022-06-27 21:47:44 +0300

    *viewsets and routers but the profile views must be generics viewsets

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render, redirect
 from requests import request
 
 # DRF
-from rest_framework import generics, status
+from rest_framework import generics, status, mixins
 from rest_framework.response import Response
 from rest_framework.generics import get_object_or_404
+# from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.viewsets import GenericViewSet
 from accounts.api.serializers import ProfileSerializer
+
 # Models
 from django.contrib.auth.models import User
 from accounts.models import Profile
@@ -14,16 +17,21 @@ from rest_framework import permissions
 from accounts.api.permissions import IsProfileRecordOwner
 from rest_framework.authentication import TokenAuthentication
 
-class ProfileListView(generics.ListAPIView):
-
-    serializer_class = ProfileSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_queryset(self):
-        return Profile.objects.filter(user=self.request.user.id)
-    
-
-class ProfileDetailView(generics.RetrieveUpdateAPIView):
+class ProfileViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    GenericViewSet
+):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [permissions.IsAuthenticated, IsProfileRecordOwner]
+
+    # def get_queryset(self):
+    #     return 
+    
+
+# class ProfileDetailView(generics.RetrieveUpdateAPIView):
+#     queryset = Profile.objects.all()
+#     serializer_class = ProfileSerializer
+#     permission_classes = [permissions.IsAuthenticated, IsProfileRecordOwner]
